b1fa6f826e4dc5cca1c3a2df6756f564436699ee
Stephanie Bridges <stephanie@digitalpolygon.com>
2024-04-15 14:45:34 -0400

    Add entity bundle class to member nodes to facilitate getting group for node.

---
+++ web/modules/custom/react_example_block/src/Entity/Node/Member.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types = 1);
+
+namespace Drupal\react_example_block\Entity\Node;
+
+use Drupal\group\Entity\GroupInterface;
+use Drupal\group\Entity\GroupRelationship;
+use Drupal\node\Entity\Node;
+
+/**
+ * A bundle class for node entities.
+ */
+final class Member extends Node {
+
+  public function getGroup(): ?GroupInterface {
+    $group_relationship_entities = GroupRelationship::loadByEntity($this);
+
+    return ($group_relationship_entities) ? reset($group_relationship_entities) : NULL;
+  }
+
+}
